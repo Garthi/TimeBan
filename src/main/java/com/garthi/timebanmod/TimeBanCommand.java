852619905d12065d309821affe04fc28fce42ee7
Garthi <garth@new-crusader.de>
2018-06-11 23:14:47 +0200

    add command base

--- src/main/java/com/garthi/timebanmod/TimeBanCommand.java
+++ src/main/java/com/garthi/timebanmod/TimeBanCommand.java
@@ -1,11 +1,16 @@
 package com.garthi.timebanmod;
 
+import com.mojang.authlib.Agent;
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.ProfileLookupCallback;
+import com.typesafe.config.ConfigException;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.fml.common.FMLLog;
 
 /**
@@ -25,20 +30,58 @@ public class TimeBanCommand extends CommandBase
     @Override
     public String getUsage(ICommandSender sender)
     {
-        return String.format("/%s <name>", COMMAND_NAME);
+        return String.format("/%s <name> <minutes>", COMMAND_NAME);
     }
 
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args)
             throws CommandException
     {
-        if (args != null && args.length < 1) {
-            return;
-        }
-
         try {
             if (sender instanceof EntityPlayer) {
-                assert args != null;
+                if (args == null || args.length < 2) {
+                    sender.sendMessage(new TextComponentTranslation("time.ban.command.time.ban.help", COMMAND_NAME));
+                    return;
+                }
+                
+                int banTime = 0;
+                String playerName = args[0]; 
+                
+                // check params
+                
+                // get player object
+                ProfileLookupCallback profilelookupcallback = new ProfileLookupCallback()
+                {
+                    public void onProfileLookupSucceeded(GameProfile p_onProfileLookupSucceeded_1_)
+                    {
+                        // TODO: kick player
+                        
+                        // feedback
+                        sender.sendMessage(new TextComponentString("Spieler " + playerName + " wurde gefunden"));
+                    }
+                    public void onProfileLookupFailed(GameProfile p_onProfileLookupFailed_1_, Exception p_onProfileLookupFailed_2_)
+                    {
+                        // feedback for fail
+                        sender.sendMessage(new TextComponentString("Spieler " + playerName + " wurde nicht gefunden"));
+                    }
+                };
+                
+                try {
+                    String[] players = new String[]{playerName};
+                    assert server.getServer() != null;
+                    server.getServer().getGameProfileRepository().findProfilesByNames(players, Agent.MINECRAFT, profilelookupcallback);
+                } catch (NullPointerException e) {
+                    FMLLog.log.catching(e);
+                }
+                
+                // ban player
+                try {
+                    ConfigHelper.player(playerName).add(banTime);
+                } catch (NotLoadedException e) {
+                    FMLLog.log.catching(e);
+                }
+                
+                // feedback to op
                 sender.sendMessage(new TextComponentString("Hello " + args[0]));
             } else {
                 assert sender != null;
