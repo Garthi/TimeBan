3e71a3547fca1c2df6f86851d0f1076d40abee4a
Garthi <garth@new-crusader.de>
2018-06-22 07:31:10 +0200

    add ban list command

---
+++ src/main/java/com/garthi/timebanmod/commands/ListBanCommand.java
@@ -0,0 +1,43 @@
+package com.garthi.timebanmod.commands;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.TextComponentString;
+
+/**
+ * @author Martin "Garth" Zander <garth@new-crusader.de>
+ * @package BanMod
+ */
+public class ListBanCommand extends CommandBase
+{
+    private static final String COMMAND_NAME = "listban";
+    
+    @Override
+    public String getName()
+    {
+        return COMMAND_NAME;
+    }
+
+    @Override
+    public String getUsage(ICommandSender sender)
+    {
+        return String.format("/%s", COMMAND_NAME);
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
+    {
+        if (sender instanceof EntityPlayer) {
+            sender.sendMessage(new TextComponentString(
+                    EntityPlayer.EnumChatVisibility.SYSTEM + "Liste der gebannten spieler"
+            ));
+        } else {
+            sender.sendMessage(new TextComponentString(
+                    EntityPlayer.EnumChatVisibility.SYSTEM + "This command is not available inside the console."
+            ));
+        }
+    }
+}
